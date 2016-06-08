0db12f8389da0fa78b5c8f7469a4b4ac7789362b
yuri <e.condori.g@gmail.com>
2016-06-08 10:37:31 -0500

    add selection, paginator

--- js/maps.js
+++ js/maps.js
@@ -2,7 +2,7 @@
  * Created by Yuri on 26/05/2016.
  */
     /*
-    centrar default,apagar ventana flotante en cambio de direccion,paginador, sesion
+    ,apagar ventana flotante en cambio de direccion,, sesion
      */
 //var pinColor = "FE7569";
 var path_coords = [{
@@ -23,7 +23,6 @@ var mapPosition =function(options,data){
     this.url=options.url;
     this.data = data;
     this.params = {header:null,body:null};
-    //this.queryToken();
 }
 mapPosition.prototype.queryToken=function(){
     var date = this.convertDate(new Date());
@@ -143,93 +142,3 @@ mapPosition.prototype.resize= function (map) {
     map.fitBounds(path_bounds);
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-function initialize() {
-    var data = {'codigo':5};
-    var options= {'url':'testurl'};
-    new mapPosition(options,data);
-    map_center = new google.maps.LatLng(-16.407009, -71.520284);
-    var mapCanvas = document.getElementById('map');
-    var mapOptions = {
-        center: map_center,
-        zoom: 12,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    map = new google.maps.Map(mapCanvas, mapOptions);
-    addRoute(map);
-}
-
-function addMarker(label, location, map,pinColor) {
- /*   var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
-        new google.maps.Size(21, 34),
-        new google.maps.Point(0,0),
-        new google.maps.Point(10, 34));
-
-    var marker = new google.maps.Marker({
-        position: location,
-        //label: label,// numeration
-        map: map,
-        icon: pinImage
-        //shadow: pinShadow
-    });
-
-    var contentString = '<div id="content">'+
-        '<div id="siteNotice">'+
-        '</div>'+
-        '<h1 id="firstHeading" class="firstHeading">'+pinColor+'</h1>'+
-        '<div id="bodyContent">'+
-        '</div>'+
-        '</div>';
-
-    var infowindow = new google.maps.InfoWindow({
-        content: contentString
-    });
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-    });
-    /**/
-}
-/*
-function addRoute(map) {
-    var myArray = ["4387FD","03D4E5","E503C2","E51F03","E59203","C6E503"];
-    var eventClick = [];
-    var legend = document.getElementById('legend');
-    path_bounds = new google.maps.LatLngBounds();
-    for (var i = 0; i < path_coords.length; i++) {
-        var pinColor = myArray[Math.floor(Math.random() * myArray.length)];
-        addMarker((i + 1).toString(), path_coords[i], map,pinColor);
-        var div = document.createElement('div');
-
-        div.innerHTML = '<div id='+i+' style="background-color:#'+pinColor+'"> ' +pinColor + '</div>';
-        legend.appendChild(div);
-        eventClick[i]= $('#'+i).click({OBJ:this,latlong:path_coords[i],map:map},function(e) {
-            e.data.OBJ.selectMarker(e.data.latlong,e.data.map);
-        });
-        path_bounds.extend(
-            new google.maps.LatLng(
-                path_coords[i].lat,
-                path_coords[i].lng));
-    }
-
-    //flightPath.setMap(map);
-    resize();
-
-    google.maps.event.addDomListener(window, 'resize',
-        resize);
-
-    map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(legend);
-}
-/**/
-/*
-function resize() {
-    map.setCenter(map_center);
-    map.fitBounds(path_bounds);
-}/**/
-/*
-function selectMarker(obj,map){
-    ///console.log(lat);
-    map.setZoom(17);      //
-    map.setCenter(new google.maps.LatLng(obj.lat,obj.lng));
-    map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
-}
-/**/
-
